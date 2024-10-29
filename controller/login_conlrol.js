8bd8721825fc56de498d5cd1983640d79aa4bc6b
Muhammad Hammad Habib <85122217+Muhammad-Hammad-Habib@users.noreply.github.com>
2024-10-30 01:17:01 +0500

    hashin password

--- controller/login_conlrol.js
+++ controller/login_conlrol.js
@@ -1,4 +1,5 @@
 import user_model from "../models/user_model.js";
+import bcrypt from "bcrypt";
 
 class login_user {
   static home_page = async (req, resp) => {
@@ -11,20 +12,54 @@ class login_user {
   // create user -- registration form
   static create_user = async (req, resp) => {
     try {
+      const hashed_pass = await bcrypt.hash(req.body.password, 5);
       const userData = new user_model({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
-      })
-       await userData.save();
-       resp.redirect("/login")
+        password: hashed_pass,
+      });
+      await userData.save();
+      resp.redirect("/login");
     } catch (error) {
-      console.log(error)
+      console.log(`create_user  ${error}`);
     }
   };
-
+  // This will redirect to Login page
   static login = async (req, resp) => {
     resp.render("login");
+  };
+
+  // create user -- registration form
+  static check_user = async (req, resp) => {
+    try {
+      const { email, password } = req.body;
+      const get_data = await user_model.findOne({ email: email });
+      const check_hashed = await bcrypt.compare(password, get_data.password);
+
+      if (get_data != null) {
+        if (get_data.email === email && check_hashed) {
+          console.log("you have succesfully Logged in ");
+          resp.send(
+            `you have succesfully Logged in with this user ${get_data.email}`
+          );
+        } else {
+          if (password.trim() == "") {
+            console.log("Password required");
+            resp.send("Password required");
+          } else {
+            console.log("Inconrrect password ");
+            resp.send("Inconrrect password");
+          }
+        }
+      } else {
+        console.log("inconrrect Email");
+        resp.send(`inconrrect Email ${email}`);
+      }
+      console.log(get_data);
+      console.log(email, password);
+    } catch (error) {
+      console.log(`create_user ${error}`);
+    }
   };
 }
 
--- router/login_route.js
+++ router/login_route.js
@@ -7,8 +7,7 @@ router.get("/", login_user.home_page);
 router.get("/registration", login_user.registration);
 router.post("/registration", login_user.create_user);
 
-
-
 router.get("/login", login_user.login);
+router.post("/login", login_user.check_user);
 
 export default router;
